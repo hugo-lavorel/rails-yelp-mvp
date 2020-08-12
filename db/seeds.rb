8ae49d0ed242047e992dbf46d8ae102c08d975bb
Hugo Lavorel <hugo.lavorel@gmail.com>
2020-08-12 15:31:12 +0200

    Completed rails Yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Bristal', address: '16 villa Gaudelet', category: 'french')
+Restaurant.create(name: 'Tonu', address: '4 rue Morel', category: 'italian')
+Restaurant.create(name: 'Cheng', address: '25 rue Paradis', category: 'chinese')
+Restaurant.create(name: 'Labibi', address: '17 rue de la Biere', category: 'belgian')
+Restaurant.create(name: 'Chez Lulu', address: '3 avenue du roi', category: 'french')
